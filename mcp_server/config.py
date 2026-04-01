9db49c64a68e597584548e1b1a312409c4036d99
thomasmaerz <maerz.thomas@gmail.com>
2026-04-01 15:21:27 -0700

    feat: add user email config for is_outbound detection

--- mcp_server/config.py
+++ mcp_server/config.py
@@ -18,6 +18,8 @@ class Config:
     
     ZSTD_COMPRESSION_LEVEL = 3
     
+    USER_EMAIL_ADDRESSES: list[str] = []  # Configure with your email addresses
+    
     MCP_SERVER_NAME = "email-intelligence"
     MCP_SERVER_VERSION = "1.0.0"
     
